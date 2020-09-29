0cac259fa7c73749e4835389486d3c891718fd7f
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-09-29 13:32:23 +0200

    Upload oefening 1

---
+++ SimpleAlgorithm/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAlgorithm
+{
+    class Product
+    {
+        public double Price { get; private set; }
+        public string Name { get; private set; }
+
+        public Product()
+        {
+            Name = "";
+            Price = 0;
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
