d69df2f77f582ec300c93cddf85cbbca8f7f4dc2
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-09-29 15:06:30 +0200

    Deleted string[] args at Main(), because it was unused

--- SimpleAlgorithm/Program.cs
+++ SimpleAlgorithm/Program.cs
@@ -4,7 +4,7 @@ namespace SimpleAlgorithm
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Order order = new Order();
             order.AddProduct("Apple", 0.99);
